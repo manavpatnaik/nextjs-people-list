c572ccbd3ec4de095d31373ed8d76caf744fefc8
Manav Patnaik <manavpatnaik@hotmail.com>
2021-05-29 19:12:38 +0530

    Fetched data for People and Posts

--- pages/people/index.js
+++ pages/people/index.js
@@ -1,7 +1,28 @@
-const People = () => {
+import Link from "next/link";
+import styles from "../../styles/People.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const people = await res.json();
+
+  return {
+    props: {
+      people,
+    },
+  };
+};
+
+const People = ({ people }) => {
   return (
     <div>
       <h1>People</h1>
+      {people.map((person) => (
+        <Link href="/" key={person.id}>
+          <a className={styles.single}>
+            <h3>{person.name}</h3>
+          </a>
+        </Link>
+      ))}
     </div>
   );
 };
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,28 @@
-const Posts = () => {
+import Link from "next/link";
+import styles from "../../styles/People.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+const Posts = ({ posts }) => {
   return (
     <div>
       <h1>Posts</h1>
+      {posts.map((post) => (
+        <Link href="/" key={post.id}>
+          <a className={styles.single}>
+            <h3>{post.title}</h3>
+          </a>
+        </Link>
+      ))}
     </div>
   );
 };
