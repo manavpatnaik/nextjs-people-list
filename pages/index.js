8c83f6952cd51a209eeaa0c34564e0dd6ab59d6b
Manav Patnaik <manavpatnaik@hotmail.com>
2021-05-29 18:53:26 +0530

    Structured Home page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque laboriosam aliquam ratione explicabo fugit, omnis commodi aliquid magnam eligendi maxime, exercitationem facilis harum temporibus qui amet assumenda atque, quis quaerat!</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque laboriosam aliquam ratione explicabo fugit, omnis commodi aliquid magnam eligendi maxime, exercitationem facilis harum temporibus qui amet assumenda atque, quis quaerat!</p>
+      <h1 className={styles.title}>Home Page</h1>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+        laboriosam aliquam ratione explicabo fugit, omnis commodi aliquid magnam
+        eligendi maxime, exercitationem facilis harum temporibus qui amet
+        assumenda atque, quis quaerat!
+      </p>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
+        laboriosam aliquam ratione explicabo fugit, omnis commodi aliquid magnam
+        eligendi maxime, exercitationem facilis harum temporibus qui amet
+        assumenda atque, quis quaerat!
+      </p>
+      <Link href="/people"> 
+        <a className={styles.btn}>People</a>
+      </Link>
+      <Link href="/posts">
+        <a className={styles.btn}>Posts</a>
+      </Link>
     </div>
   );
 }
