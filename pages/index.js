9509296223ece864d8fae6e92b0ae12f70a2432d
Manav Patnaik <manavpatnaik@hotmail.com>
2021-05-29 18:38:01 +0530

    Refactored for Layout

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque laboriosam aliquam ratione explicabo fugit, omnis commodi aliquid magnam eligendi maxime, exercitationem facilis harum temporibus qui amet assumenda atque, quis quaerat!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque laboriosam aliquam ratione explicabo fugit, omnis commodi aliquid magnam eligendi maxime, exercitationem facilis harum temporibus qui amet assumenda atque, quis quaerat!</p>
     </div>
-  )
+  );
 }
