9a4ea71d38d15f5a987301c1cde846d9de627a33
Manav Patnaik <manavpatnaik@hotmail.com>
2021-05-29 18:55:07 +0530

    Added about page

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum illum in
+        eum praesentium quo, quos, placeat aliquam accusantium eius pariatur
+        voluptas vero nam deserunt officiis ab laudantium voluptatibus labore!
+        Similique.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum illum in
+        eum praesentium quo, quos, placeat aliquam accusantium eius pariatur
+        voluptas vero nam deserunt officiis ab laudantium voluptatibus labore!
+        Similique.
+      </p>
+    </div>
+  );
+};
+
+export default About;
